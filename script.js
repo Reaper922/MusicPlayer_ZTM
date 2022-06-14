c6d62c896d0b415aa850ae1ec39f522e9040b542
Dennis Ammen <dennis.ammen@outlook.com>
2022-06-14 22:33:51 +0200

    Progress bar functionality

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -71,8 +73,17 @@ function loadSong(song) {
     image.src = `img/${song.name}.jpg`;
 }
 
+function updateProgressBar(event) {
+    if(isPlaying) {
+        const { duration, currentTime } = event.srcElement;
+        const progressPercent = (currentTime / duration) * 100;
+        progress.style.width = `${progressPercent}%`;
+    }
+}
+
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('timeupdate', updateProgressBar)
 
 loadSong(songs[songIndex]);
